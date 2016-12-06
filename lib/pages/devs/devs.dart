dd64dbc6874b69a5986b551518c17c82dbbafd2c
Daniel Tal <daniel.tal@outlook.com>
2016-12-06 13:39:49 +0200

    1002

--- lib/pages/devs/devs.dart
+++ lib/pages/devs/devs.dart
@@ -1,13 +1,21 @@
 import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
 
 @Component
 (
     selector: 'mochweb-devs',
     templateUrl: 'devs.html',
-    styleUrls: const['devs.css']
+    styleUrls: const['devs.css'],
+    directives: const [materialDirectives],
+    providers: const [materialProviders]    
 )
 
 class DevsComponent
 {
-
+  int count = 0;
+  bool allowed = true;
+  void increment()
+  {
+    count++;
+  }
 }
